5d27897fc8d066692bc95e04dba606c3d5fbe010
lv <lozovoyv@gmail.com>
2022-04-22 11:13:46 +0300

    Fixes and updates

--- app/Exceptions/Base/WrongStatusException.php
+++ app/Exceptions/Base/WrongStatusException.php
@@ -3,8 +3,22 @@
 namespace App\Exceptions\Base;
 
 use InvalidArgumentException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
-class WrongStatusException extends InvalidArgumentException
+class WrongStatusException extends InvalidArgumentException implements HttpExceptionInterface
 {
+    public function __construct()
+    {
+        parent::__construct(__('Неверный статус'));
+    }
 
+    public function getStatusCode(): int
+    {
+        return 400;
+    }
+
+    public function getHeaders(): array
+    {
+        return [];
+    }
 }
--- app/Exceptions/Base/WrongTypeException.php
+++ app/Exceptions/Base/WrongTypeException.php
@@ -3,8 +3,22 @@
 namespace App\Exceptions\Base;
 
 use InvalidArgumentException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
-class WrongTypeException extends InvalidArgumentException
+class WrongTypeException extends InvalidArgumentException implements HttpExceptionInterface
 {
+    public function __construct()
+    {
+        parent::__construct(__('Неверный тип'));
+    }
 
+    public function getStatusCode(): int
+    {
+        return 400;
+    }
+
+    public function getHeaders(): array
+    {
+        return [];
+    }
 }
