88e6d9f35ca8b0e3ae8d80c38e5b42a2a4d29816
Artem Kudin <moozz1k@yandex.ru>
2023-05-16 22:18:46 +0400

    feat(18091): Заменить заголовок

--- public/js/lead-single.js
+++ public/js/lead-single.js
@@ -25868,7 +25868,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
 
-var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Записаться на разовое тренировочное занятие");
+var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Записаться на тренировочные сборы");
 
 var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   style: {
